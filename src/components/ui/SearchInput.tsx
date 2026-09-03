670bdb610d6f2f8eeb66f2eed9fc8cd91157d5af
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-09-03 17:52:43 +0530

    Update SearchInput.tsx

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import { Search } from 'lucide-react-native'
 import { Colors } from '../../constant/colors'
 
-const SearchInput = () => {
+type Props = {
+  value?: string;
+  onChangeText?: (text: string) => void;
+};
+
+const SearchInput = ({ value, onChangeText }: Props) => {
   return (
     <View style={{
       height: 48,
@@ -11,21 +16,23 @@ const SearchInput = () => {
       borderRadius: 100,
       elevation: 2,
       backgroundColor: 'white',
-      // paddingVertical: 14,
       paddingHorizontal: 16,
       flexDirection: 'row',
       alignItems: 'center',
       gap: 12
     }}>
       <Search color={Colors.ICON_GRAY} />
-      <TextInput placeholder='Search your place' style={
-        {
-          // fontSize: 16,
+      <TextInput
+        placeholder='Search your place'
+        placeholderTextColor={Colors.TEXT_GRAY}
+        value={value}
+        onChangeText={onChangeText}
+        style={{
+          flex: 1,
           color: 'black',
-          // height: 24,
           backgroundColor: 'white'
-        }
-      } />
+        }}
+      />
     </View>
   )
 }
