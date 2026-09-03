894d36cd9295e0b8d4ea9020d8d5f8008fd54774
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-09-03 17:52:50 +0530

    Update properties.ts

--- src/controllers/properties.ts
+++ src/controllers/properties.ts
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react"
-import { propertyAPI } from "../api/properties"
-import { Property } from "../types/properties"
+import { PropertyAPI } from "../api/properties"
+import { PropertyItem } from "../types/properties"
 
 export const useGetProperties = () => {
-
-  const [properties, setProperties] = useState<Property[]>([]);
-  const [page, setPage] = useState(1);
+  const [properties, setProperties] = useState<PropertyItem[]>([]);
+  const [page] = useState(1);
 
   useEffect(() => {
-    propertyAPI.getAllProperties(page).then(data => {
+    PropertyAPI.getAllProperties(page, 10, "ALL" as any, { min: 0, max: 0 }, [], "").then((data: any) => {
       console.log("properties ", data)
-      setProperties([...properties, ...data.data])
-    });
+      if (data && data.data) {
+        setProperties(data.data)
+      }
+    }).catch(err => console.error(err));
   }, [])
 
   return {
     properties
   }
-
 }
+
